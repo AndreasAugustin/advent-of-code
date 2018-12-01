add602ba1d126260edbae6a4110144ad4d8df552
Andreas Augustin <dev@andreas-augustin.org>
2018-12-01 20:24:27 +0100

    comands refactoring

--- py/src/day_01.py
+++ py/src/day_01.py
@@ -1,5 +1,27 @@
-def main():
-    print("Advent of code day ")
+from typing import List
+
+_input: List[int] = [1, -2, 10]
+
+
+def main() -> int:
+    """
+    main function
+    :rtype: int the status code
+    """
+    print("Advent of code day 01")
+    res = sum_array(_input)
+    print(f"result is: {res}")
+    return 0
+
+
+def sum_array(arr: List[int]) -> int:
+    """
+    sums all elements in the array
+    :rtype: int
+    :param arr:
+    :return:
+    """
+    return sum(arr)
 
 
 if __name__ == "__main__":
---
+++ py/tests/day_01_test.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from ..src.day_01 import sum_array
+
+
+def test_sum_array_1():
+    _input: List[int] = [+1, -2, +3, +1]
+    res = sum_array(_input)
+    assert res == 3
+
+
+def test_sum_array_2():
+    _input: List[int] = [+1, +1, +1]
+    res = sum_array(_input)
+    assert res == 3
+
+
+def test_sum_array_3():
+    _input: List[int] = [+1, +1, -2]
+    res = sum_array(_input)
+    assert res == 0
+
+
+def test_sum_array_4():
+    _input: List[int] = [-1, -2, -3]
+    res = sum_array(_input)
+    assert res == -6

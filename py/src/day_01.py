574b884e3af77affbab133a26af5d3f3030d3afb
Andreas Augustin <dev@andreas-augustin.org>
2018-12-01 22:06:20 +0100

    change for part 2

--- py/src/day_01.py
+++ py/src/day_01.py
@@ -22,19 +22,44 @@ def main() -> int:
     data_file = \
         os.path.join(script_directory, '..', '..', 'data', 'day_01_input.csv')
     _input = file_lines_to_array(data_file)
-    res = sum_array(_input)
-    print(f"result is: {res}")
+
+    first_dupl = None
+    tmp = []
+    frequency = 0
+    while first_dupl is None:
+        res = sum_iterate(frequency, _input)
+        tmp = res + tmp
+        first_dupl = first_duplicate(tmp)
+        last_element = res[-1]
+        print(f"result is: {last_element}")
+        print(f"first duplicate frequency: {first_dupl}")
+
     return 0
 
 
-def sum_array(arr: List[int]) -> int:
+def first_duplicate(a: List):
+    set_ = set()
+    for item in a:
+        if item in set_:
+            return item
+        set_.add(item)
+    return None
+
+
+def sum_iterate(start: int, arr: List[int]) -> List[int]:
     """
-    sums all elements in the array
+    sums all elements in the array iterative, returns array with the iterated values
+    last value is the actual sum
     :rtype: int
     :param arr:
     :return:
     """
-    return sum(arr)
+    a = start
+    res = []
+    for x in arr:
+        a = a + x
+        res.append(a)
+    return res
 
 
 if __name__ == "__main__":
--- py/test/day_01_test.py
+++ py/test/day_01_test.py
@@ -1,36 +1,49 @@
 #!python
 from typing import List
 
-from ..src.day_01 import sum_array, file_lines_to_array
+from ..src.day_01 import sum_iterate, file_lines_to_array, first_duplicate
+
+import os
+script_directory = os.path.dirname(os.path.realpath(__file__))
+data_file = os.path.join(script_directory, 'data', 'day_01_input.csv')
 
 
-def test_sum_array_1():
+def test_first_duplicate_1():
+    _input = [1, 3, 5, 3, 6, 8, 1]
+    res = first_duplicate(_input)
+    assert res == 3
+
+
+def test_first_duplicate_2():
+    data_arr = file_lines_to_array(data_file)
+    first_dupl = first_duplicate(data_arr)
+    assert first_dupl == -2
+
+
+def test_sum_iterate_1():
     _input: List[int] = [+1, -2, +3, +1]
-    res = sum_array(_input)
-    assert res == 3
+    res = sum_iterate(0, _input)
+    assert res[-1] == 3
 
 
-def test_sum_array_2():
+def test_sum_iterate_2():
     _input: List[int] = [+1, +1, +1]
-    res = sum_array(_input)
-    assert res == 3
+    res = sum_iterate(0, _input)
+    assert res[-1] == 3
 
 
-def test_sum_array_3():
+def test_sum_iterate_3():
     _input: List[int] = [+1, +1, -2]
-    res = sum_array(_input)
-    assert res == 0
+    res = sum_iterate(0, _input)
+    assert res[-1] == 0
 
 
-def test_sum_array_4():
+def test_sum_iterate_4():
     _input: List[int] = [-1, -2, -3]
-    res = sum_array(_input)
-    assert res == -6
+    res = sum_iterate(0, _input)
+    assert res[-1] == -6
 
 
 def test_file_lines_to_array():
-    import os
-    script_directory = os.path.dirname(os.path.realpath(__file__))
-    data_file = os.path.join(script_directory, 'data', 'day_01_input.csv')
     res = file_lines_to_array(data_file)
-    assert res == [1, -2, 4, 2]
+    assert res == [1, -2, 4, -2, 2]

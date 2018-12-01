769c6c9db3acd0aac5c8fde49564107227967b5a
Andreas Augustin <dev@andreas-augustin.org>
2018-12-01 21:15:39 +0100

    refactoring to use data files

--- py/src/day_01.py
+++ py/src/day_01.py
@@ -1,7 +1,13 @@
 #!python
 from typing import List
 
-_input: List[int] = [1, -2, 10]
+
+def file_lines_to_array(file_path: str) -> List[int]:
+    fh = open(file_path)
+    x = [int(i) for i in fh.readlines()]
+
+    fh.close()
+    return x
 
 
 def main() -> int:
@@ -10,6 +16,12 @@ def main() -> int:
     :rtype: int the status code
     """
     print("Advent of code day 01")
+
+    import os
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    data_file = \
+        os.path.join(script_directory, '..', '..', 'data', 'day_01_input.csv')
+    _input = file_lines_to_array(data_file)
     res = sum_array(_input)
     print(f"result is: {res}")
     return 0
--- py/test/day_01_test.py
+++ py/test/day_01_test.py
@@ -1,7 +1,7 @@
 #!python
 from typing import List
 
-from ..src.day_01 import sum_array
+from ..src.day_01 import sum_array, file_lines_to_array
 
 
 def test_sum_array_1():
@@ -26,3 +26,11 @@ def test_sum_array_4():
     _input: List[int] = [-1, -2, -3]
     res = sum_array(_input)
     assert res == -6
+
+
+def test_file_lines_to_array():
+    import os
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    data_file = os.path.join(script_directory, 'data', 'day_01_input.csv')
+    res = file_lines_to_array(data_file)
+    assert res == [1, -2, 4, 2]

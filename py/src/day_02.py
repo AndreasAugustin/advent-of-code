f225c9792edc5b8eea613c172db233912a8cc4f0
Andreas Augustin <dev@andreas-augustin.org>
2018-12-02 23:02:38 +0100

    day 02 part one solved

--- py/src/day_02.py
+++ py/src/day_02.py
@@ -1,10 +1,33 @@
 #!python
-from typing import Dict
+from typing import Dict, List
 import logging
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def file_lines_to_array(file_path: str) -> List[str]:
+    fh = open(file_path)
+    x = [str(i) for i in fh.readlines()]
+
+    fh.close()
+    return x
+
+
 def main() -> int:
-    logging.info("Advent of code day 01")
+    logger.info("Advent of code day 01")
+    import os
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    data_file = \
+        os.path.join(script_directory, '..', '..', 'data', 'day_02_input.csv')
+    _input = file_lines_to_array(data_file)
+
+    input_arr = file_lines_to_array(data_file)
+
+    calculated_hash = calc_hash(input_arr)
+
+    logger.info(f"Hash: {calculated_hash}")
 
     return 0
 
@@ -16,18 +39,43 @@ def check_freq(input_str: str) -> Dict[str, int]:
     return freq
 
 
-def has_exactly_two(input_str: str) -> bool:
-    return has_exactly(2, input_str)
+def has_two_equal_chars(input_str: str) -> bool:
+    return has_number_equal_chars(2, input_str)
 
 
-def has_exactly(number: int, input_str: str) -> bool:
+def has_three_equal_chars(input_str: str) -> bool:
+    return has_number_equal_chars(3, input_str)
+
+
+def has_number_equal_chars(number: int, input_str: str) -> bool:
     freq = check_freq(input_str)
-    found_once = False
     for key, value in freq.items():
         logging.debug(f"key: {key}, value: {value}")
         if value == number:
-            if found_once is True:
-                return False
-            found_once = True
+            return True
+    return False
 
-    return found_once
+
+def count_of_two_equal_chars(input_arr: List[str]) -> int:
+    res = 0
+    for s in input_arr:
+        if has_two_equal_chars(s) is True:
+            res += 1
+    return res
+
+
+def count_of_three_equal_chars(input_arr: List[str]) -> int:
+    res = 0
+    for s in input_arr:
+        if has_three_equal_chars(s) is True:
+            res += 1
+    return res
+
+
+def calc_hash(input_arr: List[str]) -> int:
+    return count_of_two_equal_chars(input_arr) \
+           * count_of_three_equal_chars(input_arr)
+
+
+if __name__ == "__main__":
+    main()
--- py/test/day_02_test.py
+++ py/test/day_02_test.py
@@ -1,6 +1,13 @@
 #!python
-from ..src.day_02 import check_freq, has_exactly_two
+from ..src.day_02 import check_freq, has_two_equal_chars, \
+    has_three_equal_chars, file_lines_to_array, \
+    count_of_three_equal_chars, count_of_two_equal_chars, \
+    calc_hash
 import logging
+
+import os
+script_directory = os.path.dirname(os.path.realpath(__file__))
+data_file = os.path.join(script_directory, 'data', 'day_02_input.csv')
 
 
 def test_check_freq_1():
@@ -45,8 +52,51 @@ def test_check_freq_7():
     assert res == {"a": 3, "b": 3}
 
 
-def test_has_exactly_two(caplog):
-    caplog.set_level(logging.DEBUG)
+def test_has_two_equal_chars_1(caplog):
+    # caplog.set_level(logging.DEBUG)
     _input = "abcdee"
-    res = has_exactly_two(_input)
+    res = has_two_equal_chars(_input)
     assert res is True
+
+
+def test_has_two_equal_chars_2():
+    _input = "ababab"
+    res = has_two_equal_chars(_input)
+    assert res is False
+
+
+def test_has_two_equal_chars_3():
+    _input = "abcdefg"
+    res = has_two_equal_chars(_input)
+    assert res is False
+
+
+def test_has_three_equal_chars_1():
+    _input = "ababab"
+    res = has_three_equal_chars(_input)
+    assert res is True
+
+
+def test_has_three_equal_chars_2(caplog):
+    # caplog.set_level(logging.DEBUG)
+    _input = "aaabcd"
+    res = has_three_equal_chars(_input)
+    assert res is True
+
+
+def test_count_has_two_equal_chars():
+    data_arr = file_lines_to_array(data_file)
+    res = count_of_two_equal_chars(data_arr)
+    assert res == 4
+
+
+def test_count_has_three_equal_chars():
+    data_arr = file_lines_to_array(data_file)
+    res = count_of_three_equal_chars(data_arr)
+    assert res == 3
+
+
+def test_calc_hash():
+    data_arr = file_lines_to_array(data_file)
+    res = calc_hash(data_arr)
+    assert res == 12

9157613cc1e6c9f25275c7367a17fdc3a8b85267
Andreas Augustin <dev@andreas-augustin.org>
2018-12-04 22:51:41 +0100

    day 01 test refactoring -> parameterized tests

--- py/test/day_01_test.py
+++ py/test/day_01_test.py
@@ -1,47 +1,32 @@
 #!python
 from typing import List
-
+import pytest
 from ..src.day_01 import sum_iterate, file_lines_to_array, first_duplicate
 
 import os
+
 script_directory = os.path.dirname(os.path.realpath(__file__))
 data_file = os.path.join(script_directory, 'data', 'day_01_input.csv')
 
 
-def test_first_duplicate_1():
-    _input = [1, 3, 5, 3, 6, 8, 1]
-    res = first_duplicate(_input)
-    assert res == 3
+@pytest.mark.parametrize("dup_input, dup_exp", [
+    [[1, 3, 5, 3, 6, 8, 1], 3],
+    [file_lines_to_array(data_file), -2]
+])
+def test_first_duplicate(dup_input, dup_exp):
+    res = first_duplicate(dup_input)
+    assert res == dup_exp
 
 
-def test_first_duplicate_2():
-    data_arr = file_lines_to_array(data_file)
-    first_dupl = first_duplicate(data_arr)
-    assert first_dupl == -2
-
-
-def test_sum_iterate_1():
-    _input: List[int] = [+1, -2, +3, +1]
-    res = sum_iterate(0, _input)
-    assert res[-1] == 3
-
-
-def test_sum_iterate_2():
-    _input: List[int] = [+1, +1, +1]
-    res = sum_iterate(0, _input)
-    assert res[-1] == 3
-
-
-def test_sum_iterate_3():
-    _input: List[int] = [+1, +1, -2]
-    res = sum_iterate(0, _input)
-    assert res[-1] == 0
-
-
-def test_sum_iterate_4():
-    _input: List[int] = [-1, -2, -3]
-    res = sum_iterate(0, _input)
-    assert res[-1] == -6
+@pytest.mark.parametrize("it_input, it_exp", [
+    [[+1, -2, +3, +1], 3],
+    [[+1, +1, +1], 3],
+    [[+1, +1, -2], 0],
+    [[-1, -2, -3], -6]
+])
+def test_sum_iterate(it_input, it_exp):
+    res = sum_iterate(0, it_input)
+    assert res[-1] == it_exp
 
 
 def test_file_lines_to_array():
